c10a61f2c9c620704ef9a53a67da861503c8af9d
JackF710 <jtfeery@gmail.com>
2025-01-31 14:00:18 -0500

    bread

---
+++ app/src/components/BreadList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2 v-for="bread in breads" :key="bread">{{ bread }}</h2>
+  </div>
+</template>
+
+<script setup>
+const breads = [
+  'Wheat Bread',
+  'White Bread',
+  'Italian Herb & Cheese',
+  'Malted Rye',
+  'Plain Wrap',
+  'Gluten Free Wrap',
+  'Multigrain Wrap',
+]
+</script>
+
+<style scoped></style>
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import BreadList from '../components/BreadList.vue'
+defineProps({
+  bread: Object,
+})
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Choose a Bread</h1>
+    <h3>{{ breads }}</h3>
   </main>
 </template>
