5839e3dddd3a5985f84466ecb5561eadaa215949
JackF710 <jtfeery@gmail.com>
2025-02-06 13:59:36 -0500

    tspmo

---
+++ app/src/components/ToppingCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>{{ bread.name }}</h2>
+  </div>
+</template>
+
+<script setup>
+import ToppingsList from './ToppingsList.vue'
+defineProps({
+  bread: Object,
+  meat: Object,
+  cheese: Object,
+  condiment: Object,
+  vegetable: Object,
+})
+</script>
+
+<style scoped></style>
