7464c21a4c7cadac6681a1933cb5774250ab83e0
JackF710 <jtfeery@gmail.com>
2025-02-12 14:01:30 -0500

    df

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,5 +1,12 @@
 <script setup>
 import ToppingsList from '@/components/ToppingsList.vue'
+import ToppingCard from '@/components/ToppingCard.vue'
+import breads from '@/components/ToppingsList.vue'
 </script>
 
-<template></template>
+<template>
+  <div class="Card">
+    <img :src="topping.image" :alt="topping.name" class="topping-image" />
+    <h3>{{ topping.name }}</h3>
+  </div>
+</template>
