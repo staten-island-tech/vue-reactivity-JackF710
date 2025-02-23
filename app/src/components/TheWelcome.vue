a201a9f98f16b0c105b5f6e205aa48483487fd03
JackF710 <jtfeery@gmail.com>
2025-02-23 11:35:47 -0500

    g

---
+++ app/src/components/TheWelcome.vue
@@ -0,0 +1,14 @@
+<script setup>
+import ToppingsList from '@/components/ToppingsList.vue'
+import ToppingCard from '@/components/ToppingCard.vue'
+import breads from '@/components/ToppingsList.vue'
+
+
+</script>
+
+<template>
+  <div class="Card">
+    <img :src="topping.image" :alt="topping.name" class="topping-image" />
+    <h3>{{ topping.name }}</h3>
+  </div>
+</template>
